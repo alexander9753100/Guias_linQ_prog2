a86ea741ba5c79667ea8b9f2878a8a27f7c8c1db
Alexander_pineda <126535792+alexander9753100@users.noreply.github.com>
2024-09-06 05:50:58 +0300

    creacion en program.cs listaModelos, listaCasa, listaHabitantes, sentenciasLinq

--- linqGuias/Program.cs
+++ linqGuias/Program.cs
@@ -1,2 +1,114 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using linqGuias;
+
 Console.WriteLine("Hello, World!");
+
+#region ListaModelos
+List<Casa> ListaCasas = new List<Casa>();
+List<Habitante> ListaHabitantes = new List<Habitante>();
+#endregion
+
+#region listaCasa
+ListaCasas.Add(new Casa
+{
+    Id = 1,
+    Direccion = "3 av Norte ArcanCity",
+    Ciudad = "Gothan City",
+    numeroHabitaciones = 20,
+});
+ListaCasas.Add(new Casa
+{
+    Id = 2,
+    Direccion = "6 av Sur SmollVille",
+    Ciudad = "Metropolis",
+    numeroHabitaciones = 5,
+});
+ListaCasas.Add(new Casa
+{
+    Id = 3,
+    Direccion = "Forest Hills, Queens, NY 11375",
+    Ciudad = "New York"
+});
+#endregion
+
+#region ListaHabitante
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Bruno Diaz",
+    Edad = 36,
+    IdCasa = 1
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 2,
+    Nombre = "Clark Kent.",
+    Edad = 40,
+    IdCasa = 2
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 3,
+    Nombre = "Peter Parker",
+    Edad = 25,
+    IdCasa = 3
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 3,
+    Nombre = "Tia Mey",
+    Edad = 85,
+    IdCasa = 3
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 2,
+    Nombre = "Luise Lain",
+    Edad = 40,
+    IdCasa = 2
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Selina Kyle",
+    Edad = 30,
+    IdCasa = 1
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Alfred",
+    Edad = 65,
+    IdCasa = 1
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Nathan Drake",
+    Edad = 37,
+    IdCasa = 1
+});
+#endregion
+
+#region SentenciasLinQ
+IEnumerable<Habitante> ListaEdad = from ObjetoProvicional
+                                   in ListaHabitantes
+                                   where ObjetoProvicional.Edad > 40
+                                   select ObjetoProvicional;
+
+foreach (Habitante objetoProcicional2 in ListaEdad)
+{
+    Console.WriteLine(objetoProcicional2.datosHabitante());
+}
+
+//Join
+IEnumerable<Habitante> listaCasaGothan = from objetoTemporalHabitante in ListaHabitantes
+                                         join objetoTemporalCasa in ListaCasas
+                                         on objetoTemporalHabitante.IdHabitante equals objetoTemporalCasa.Id
+                                         where objetoTemporalCasa.Ciudad   == "Gothan City"
+                                         select objetoTemporalHabitante;
+Console.WriteLine("----------------------------------------------------------------------------------------------");
+foreach (Habitante h in listaCasaGothan) {
+    Console.WriteLine (h.datosHabitante());
+}
+#endregion
